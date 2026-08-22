c9ac19c5aa1cda863c5246913699c4cef3cb44ec
cliffmieser <cliffmieser@gmail.com>
2026-08-22 15:29:42 -0400

    start of basepath configuration for git pages

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from get_copies import get_copies 
+import sys
 # from generate_page import generate_page
 from generate_pages_recursive import generate_pages_recursive
 
 
 def main():
     # run get_copies function
+    basepath = "/" if not sys.argv[1] else sys.argv[1] # first CLI arguments 
+
     get_copies() 
 
     # generate the page 
